eed19223d4911dfab1d742fafc969782761f217a
Shelyo <burgev@usi.ch>
2018-07-20 11:13:06 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
+import { DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+
+ReactDOM.render(
+  <div style={{ margin: 24 }}>
+    <Button type="primary">Test</Button>
+  </div>,
+  document.getElementById("root")
+);
